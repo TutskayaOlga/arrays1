54910edb0859372367fbae705c8cb082d86078d5
olgatut <lelka.tutskaya@yandex.ru>
2022-10-12 23:18:31 +0300

    Initial commit

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-    task1();
-    }
-    public static void task1() {
-        int [] vois = new int [4];
-        System.out.println(vois);
-    }
-}
